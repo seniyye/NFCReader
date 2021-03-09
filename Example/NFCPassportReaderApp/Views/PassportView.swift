50ded2f1890efbbf4aa3b5fd5b744942cd4a61f6
fsguler <fsguler@turksat.com.tr>
2021-03-09 11:12:46 +0300

    first commit

---
+++ Example/NFCPassportReaderApp/Views/PassportView.swift
@@ -0,0 +1,84 @@
+
+//
+//  PassportView.swift
+//  NFCPassportReaderApp
+//
+//  Created by Andy Qua on 30/06/2019.
+//  Copyright © 2019 Andy Qua. All rights reserved.
+//
+
+import SwiftUI
+import NFCPassportReader
+
+struct PassportView : View {
+    @EnvironmentObject var settings: SettingsStore
+    @State private var showExportPassport : Bool = false
+    
+    var body: some View {
+        VStack {
+            NavigationLink( destination: ExportPassportView(), isActive: $showExportPassport) { Text("") }
+
+            PassportSummaryView(passport:settings.passport!)
+            HStack {
+
+                Button(action: {showExportPassport.toggle()}) {
+                    Label("Export passport", systemImage: "square.and.arrow.up")
+                }
+                .padding()
+                Spacer()
+                Button(action: {shareLogs()}) {
+                    Label("Share logs", systemImage: "square.and.arrow.up")
+                }
+                .padding()
+            }
+            DetailsView(passport:settings.passport!)
+        }
+        .navigationTitle("Passport Details")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+extension PassportView {
+    func shareLogs() {
+        hideKeyboard()
+        do {
+            let arr = Log.logData
+            let data = try JSONSerialization.data(withJSONObject: arr, options: .prettyPrinted)
+            
+            let temporaryURL = URL(fileURLWithPath: NSTemporaryDirectory() + "passportreader.log")
+            try data.write(to: temporaryURL)
+            
+            let av = UIActivityViewController(activityItems: [temporaryURL], applicationActivities: nil)
+            UIApplication.shared.windows.first?.rootViewController?.present(av, animated: true, completion: nil)
+        } catch {
+            print( "ERROR - \(error)" )
+        }
+    }
+}
+
+#if DEBUG
+struct PassportView_Previews : PreviewProvider {
+    static var previews: some View {
+        
+        let passport : NFCPassportModel
+        if let file = Bundle.main.url(forResource: "passport", withExtension: "json"),
+           let data = try? Data(contentsOf: file),
+           let json = try? JSONSerialization.jsonObject(with: data, options: []),
+           let arr = json as? [String:String] {
+            passport = NFCPassportModel(from: arr)
+        } else {
+            passport = NFCPassportModel()
+        }
+        let settings = SettingsStore()
+        settings.passport = passport
+        
+        return NavigationView {
+            PassportView()
+                .environmentObject(settings)
+                .environment( \.colorScheme, .light)
+                .navigationTitle("WEEE")
+                .navigationBarTitleDisplayMode(.inline)
+        }
+    }
+}
+#endif
---
+++ NFCPassportReader.podspec
@@ -0,0 +1,28 @@
+Pod::Spec.new do |spec|
+
+  spec.name         = "NFCPassportReader"
+  spec.version      = "1.1.1"
+  spec.summary      = "This package handles reading an NFC Enabled passport using iOS 13 CoreNFC APIS"
+
+  spec.homepage     = "https://github.com/AndyQ/NFCPassportReader"
+  spec.license      = "MIT"
+  spec.author       = { "Andy Qua" => "andy.qua@gmail.com" }
+  spec.platform = :ios
+  spec.ios.deployment_target = "12.0"
+
+  spec.source       = { :git => "https://github.com/AndyQ/NFCPassportReader.git", :tag => "#{spec.version}" }
+
+  spec.source_files  = "Sources/**/*.{swift}"
+
+  spec.swift_version = "5.0"
+
+  spec.dependency "OpenSSL-Universal", '1.1.180'
+  spec.xcconfig          = { 'OTHER_LDFLAGS' => '-weak_framework CryptoKit -weak_framework CoreNFC',
+                             'ENABLE_BITCODE' => '"NO' }
+
+  spec.pod_target_xcconfig = {
+    'EXCLUDED_ARCHS[sdk=iphonesimulator*]' => 'arm64'
+  }
+  spec.user_target_xcconfig = { 'EXCLUDED_ARCHS[sdk=iphonesimulator*]' => 'arm64' }
+
+end
